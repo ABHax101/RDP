1c90384f79674dcf56196c2c58c47c34c5cc86ec
ABHax101 <72935354+ABHax101@users.noreply.github.com>
2021-06-04 21:30:09 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator CopenHagen /add >nul
+net user administrator C0p3nHag3n /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -16,6 +16,6 @@ echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: CopenHagen
+echo Password: C0p3nHag3n
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
